7bbc8f6081d512edcd194d0aa0cd42c54a9f5228
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 16:51:14 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,7 +8,10 @@ export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      options: {
+        checks: ['none']
+      }
     })
   ],
   callbacks: {
