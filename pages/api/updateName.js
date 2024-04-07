c9e7490cf1b385bcf7d92738664d865a43a49943
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 20:13:21 +0800

    Update updateName.js

--- pages/api/updateName.js
+++ pages/api/updateName.js
@@ -1,21 +1,33 @@
-// pages/api/updateName.js
 import { getSession } from 'next-auth/react'
-import prisma from '../../lib/prisma'
+import { prisma } from '../../lib/prisma'
 
 export default async function handler(req, res) {
   const session = await getSession({ req })
+
   if (!session) {
-    return res.status(401).json({ error: 'Unauthorized' })
+    return res.status(401).json({ message: 'Unauthorized.' })
   }
 
+  const currentUser = await prisma.user.findUnique({
+    where: {
+      email: session.user.email,
+    },
+  })
+
   if (req.method === 'PUT') {
-    const { name } = req.body
+    const { newName } = req.body
+
     const updatedUser = await prisma.user.update({
-      where: { email: session.user.email },
-      data: { name },
+      where: {
+        id: currentUser.id,
+      },
+      data: {
+        name: newName,
+      },
     })
-    res.status(200).json(updatedUser)
+
+    return res.status(200).json(updatedUser)
   } else {
-    res.status(405).json({ error: 'Method not allowed' })
+    return res.status(405).json({ message: 'Method Not Allowed' })
   }
 }
