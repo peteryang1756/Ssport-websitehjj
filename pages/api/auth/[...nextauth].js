0ace2fdcd0051836fe79b18b1b0848a947938b94
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-07 16:15:05 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,16 +1,20 @@
 import NextAuth from "next-auth"
-import Providers from "next-auth/providers"
+import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import GoogleProvider from "next-auth/providers/google";
-const prisma = new PrismaClient()
+import prisma from "../../../lib/prisma"
 
 export default NextAuth({
-  providers: [
-    Providers.Google({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    }),
-  ],
   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET
+    })
+  ],
+  callbacks: {
+    async session({ session, user }) {
+      session.user.id = user.id
+      return session
+    }
+  }
 })
