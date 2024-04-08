afac1ec5dfcbe655af0353490c3e3255914a7216
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-08 22:29:27 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,20 +4,32 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import prisma from "../../../lib/prisma"
 
 export const authOptions = {
-  adapter: PrismaAdapter({ prisma }),
+  adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      authorizationParams: {}, 
+      authorizationParams: {},
       checks: ['none'],
     })
   ],
   callbacks: {
-    async session({ session, user }) {
-      session.user.id = user.id;
-      session.user.isActive = user.isActive;
-      return session;
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id
+      }
+      return token
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.id = token.id
+        const user = await prisma.user.findUnique({
+          where: { id: token.id },
+          select: { isActive: true }, // 只选择 isActive 字段
+        })
+        session.user.isActive = user?.isActive || false // 将 isActive 附加到 session.user 对象上
+      }
+      return session
     }
   }
 }
