b99ef7cd3e00af8f434a923cb8b2f3db7bde0c75
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-09 23:53:19 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -165,13 +165,13 @@ export default function Blog({posts}){
             //JSX for individual blog listing
             return <article className="bg-white dark:bg-gray-900" key={title}>
         
-     <Link className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]" href={`/blog/tags/${tags}`}>
+     <Link className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]" href={`/blog/`}>
   <div className="aspect-w-16 aspect-h-11">
     <img className="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description" />
   </div>
   <div className="my-6">
     <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
-      {tittle}
+      {title}
     </h3>
     <p className="mt-5 text-gray-600 dark:text-gray-400">
       {seo}
