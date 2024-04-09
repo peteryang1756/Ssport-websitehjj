7d55910e740a5d4bb3eb7b792ecf8ae02ab78753
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-09 23:50:36 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -165,36 +165,25 @@ export default function Blog({posts}){
             //JSX for individual blog listing
             return <article className="bg-white dark:bg-gray-900" key={title}>
         
-       <div className="w-full px-4 md:w-1/2 lg:w-1/3">
-  <div className="mb-10 w-full">
-    <div className="mb-8 overflow-hidden rounded">
-      <img src={bannerImage} alt="" className="w-full" />
-    </div>
+     <Link className="group flex flex-col h-full border border-gray-200 hover:border-transparent hover:shadow-lg transition-all duration-300 rounded-xl p-5 dark:border-gray-700 dark:hover:border-transparent dark:hover:shadow-black/[.4]" href={`/blog/tags/${tags}`}>
+  <div className="aspect-w-16 aspect-h-11">
+    <img className="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633114128174-2f8aa49759b0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="Image Description" />
+  </div>
+  <div className="my-6">
+    <h3 className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:group-hover:text-white">
+      {tittle}
+    </h3>
+    <p className="mt-5 text-gray-600 dark:text-gray-400">
+      {seo}
+    </p>
+  </div>
+  <div className="mt-auto flex items-center gap-x-3">
+    <img className="size-8 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Image Description" />
     <div>
-      
-        <>
-          <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose text-white">
-            {date}
-          </span>
-          <div className="tags">
-            {/* Ajoutez vos tags ici */}
-          </div>
-        </>
-     
-      <h3>
-        <Link
-          href={`/blog/tags/${tags}`}
-          className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
-        >
-          {tittle}
-        </Link>
-      </h3>
-      <p className="text-base text-body-color dark:text-dark-6">
-        {seo}
-      </p>
+      <h5 className="text-sm text-gray-800 dark:text-gray-200"> {author}</h5>
     </div>
   </div>
-</div>
+</Link>
 </article>
 
         })}
