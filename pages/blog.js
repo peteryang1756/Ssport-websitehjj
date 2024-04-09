343047ef02d2ef08ebad45ed9465c93060e4ddbd
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-09 23:41:26 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -165,61 +165,35 @@ export default function Blog({posts}){
             //JSX for individual blog listing
             return <article className="bg-white dark:bg-gray-900" key={title}>
         
-         <div className="flex justify-between px-4 mx-auto max-w-8xl">
-  <div className="hidden mb-6 xl:block lg:w-80">
-    <div className="sticky top-36">
+       <div className="w-full px-4 md:w-1/2 lg:w-1/3">
+  <div className="mb-10 w-full">
+    <div className="mb-8 overflow-hidden rounded">
+      <img src={image} alt="" className="w-full" />
     </div>
-  </div>
-  <div className="w-full max-w-2xl mx-auto">
-    <div className="divide-y divide-gray-200 dark:divide-gray-700">
-      <article className="py-6">
-        <div className="flex items-center justify-between mb-3 text-gray-500">
-          <div>
-            <Link
-              className="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 hover:bg-blue-200 dark:hover:bg-blue-300 dark:text-blue-800 mb-2"
-              href={`/blog/tags/${tags}`}
-            >
-              #{tags}
-            </Link>
-          </div>
-          <span className="text-sm">
+    <div>
+      
+        <>
+          <span className="mb-5 inline-block rounded bg-primary px-4 py-1 text-center text-xs font-semibold leading-loose text-white">
             {date}
           </span>
-        </div>
-    <h2 className={`mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white `}>
-    <Link href={`/blog/${slug}`}>
-            {title}
-          </Link>
-        </h2>
-        <p className="mb-5 text-gray-500 dark:text-gray-400">
-          {seo}
-        </p>
-        <div className="flex items-center justify-between">
-          <a
-            className="flex items-center space-x-2"
-            href="https://discuss.ssangyongsports.org/members/peter-yang.1/"
-          >
-            <Image
-  className="rounded-full w-7 h-7"
-  src={img}
-  alt={author}
-/>
-            
-            <span className="font-medium dark:text-white">
-               {author}
-            </span>
-          </a>
-          <Link
-            href={`/blog/${slug}`}
-          >
-                    閱讀文章
-          
-          </Link>
-        </div>
-      </article>
+          <div className="tags">
+            {/* Ajoutez vos tags ici */}
+          </div>
+        </>
+     
+      <h3>
+        <Link
+          href={`/blog/tags/${tags}`}
+          className="mb-4 inline-block text-xl font-semibold text-dark hover:text-primary dark:text-white sm:text-2xl lg:text-xl xl:text-2xl"
+        >
+          {tittle}
+        </Link>
+      </h3>
+      <p className="text-base text-body-color dark:text-dark-6">
+        {seo}
+      </p>
     </div>
-                        
-</div>
+  </div>
 </div>
 </article>
 
