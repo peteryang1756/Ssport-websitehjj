3ffbaabbcdbbd220f692ea1a3c42d3f6861f6f75
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-10 18:01:07 +0800

    Update supportend.tsx

--- pages/components/supportend.tsx
+++ pages/components/supportend.tsx
@@ -21,12 +21,12 @@ function supportend() {
           >
             聯絡我們
           </a>
-          <Link
+          <div
             className="inline-flex h-10 items-center justify-center rounded-md border border-gray-200 border-gray-200 bg-white px-8 text-sm font-medium shadow-sm transition-colors hover:bg-gray-100 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300"
             onClick={() => window.$crisp && window.$crisp.push(['do', 'chat:open'])}
           >
             在線支援
-          </Link>
+          </div>
         </div>
       </div>
     </section>
