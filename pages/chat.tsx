30a3854431190144a204eb7cfe05a1b4df7dbb0c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-11 00:30:31 +0800

    Update chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -1,7 +1,6 @@
 'use client';
  
-import { useChat } from 'ai/react';
- 
+import { useChat } from './api/chat';
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
  
