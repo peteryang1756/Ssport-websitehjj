cf25a3ca21ccc6df0dd572257349caba4f33123e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-10 18:11:38 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,31 +1,61 @@
-import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma";
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
+import LineProvider from "next-auth/providers/line"
+import DiscordProvider from "next-auth/providers/discord"
+
+// 管理员邮箱地址列表
+const adminEmails = ['ssangyongsports1@gmail.com', 'admin@example.com']
 
 export const authOptions = {
-  adapter: PrismaAdapter({ prisma }),
+  // 配置一个或多个认证提供者
   providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-      authorizationParams: {}, // 修改為 authorizationParams
+      clientId: process.env.Google_ID,
+      clientSecret: process.env.Google_SECRET,
+      authorization: {
+        params: {},
+      },
       checks: ['none'],
-    })
+    }),
+    LineProvider({
+      clientId: process.env.Line_ID,
+      clientSecret: process.env.Line_SECRET,
+    }),
+    DiscordProvider({
+      clientId: process.env.Discord_ID,
+      clientSecret: process.env.Discord_SECRET,
+    }),
   ],
-  session: {
-    strategy: 'jwt',
+  theme: {
+    logo: "/logo.png", // 图片的绝对URL
   },
   callbacks: {
-    async session({ session, user }) {
-      session.user.id = user.id;
-      return session;
-    }
-  }
-};
+    async jwt({ token, account, user }) {
+      // 在登录后将访问令牌持久化到令牌中
+      if (account) {
+        token.accessToken = account.access_token
+      }
 
-export default NextAuth(authOptions);
+      // 根据用户信息赋予角色
+      if (user && user.email) {
+        const isAdmin = adminEmails.includes(user.email)
+        token.role = isAdmin ? 'admin' : 'user'
+      }
 
-export const getServerSession = async (req, res, next) => {
-  return await NextAuth.getServerSession(req, res);
-};
+      return token
+    },
+    async session({ session, token }) {
+      // 向客户端发送访问令牌等属性
+      session.accessToken = token.accessToken
+      session.user.role = token.role
+      return session
+    },
+  },
+}
+
+export default NextAuth(authOptions)
