d0c2e8b50e961dbd5cd59df5688d5bfcd12a1d86
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-11 00:43:32 +0800

    Update route.ts

--- pages/api/chat/route.ts
+++ pages/api/chat/route.ts
@@ -1,11 +1,11 @@
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
- 
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
- 
+
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge';
- 
+
 // convert messages from the Vercel AI SDK Format to the format
 // that is expected by the Google GenAI SDK
 const buildGoogleGenAIPrompt = (messages: Message[]) => ({
@@ -16,18 +16,18 @@ const buildGoogleGenAIPrompt = (messages: Message[]) => ({
       parts: [{ text: message.content }],
     })),
 });
- 
+
 export async function POST(req: Request) {
   // Extract the `prompt` from the body of the request
   const { messages } = await req.json();
- 
+
   const geminiStream = await genAI
     .getGenerativeModel({ model: 'gemini-pro' })
     .generateContentStream(buildGoogleGenAIPrompt(messages));
- 
+
   // Convert the response into a friendly text-stream
   const stream = GoogleGenerativeAIStream(geminiStream);
- 
+
   // Respond with the stream
   return new StreamingTextResponse(stream);
 }
