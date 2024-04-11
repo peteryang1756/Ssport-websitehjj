005bfd479f1406fd55be64be3881c882360c20c3
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-11 18:23:54 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -7,15 +7,17 @@ import '@docsearch/css';
 function supporthero() {
   return (
 <section
-  className="bg-gradient-to-r from-purple-600 via-blue-500 to-indigo-900 py-20"
+  className="bg-gradient-to-r from-purple-600 via-blue-500 to-indigo-900 py-30"
 >
   <div className="py-12 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">
       需要 <span className={styles.c}>雙龍體育</span>的幫助嗎?
     </h2>
+    <div className="pt-9">
     <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 xl:px-48">
       我們提供全面的體育服務和支援,讓您輕鬆享受各種運動項目。無論您是專業運動員還是業餘愛好者,我們都能為您量身打造合適的解決方案。
     </p>
+      </div>
     <div className={`${a.docSearch} ${a.container}`}>
       <DocSearch
         apiKey="f17332c3a1c0389e31da9845b6a6bdf1"
