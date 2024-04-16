6c150ccdb0201b828d04e4529f5d60c89c50dd02
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-16 23:41:26 +0800

    Update contentlayer.config.ts

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,53 @@
-import { makeSource } from 'contentlayer/source-files'
+import { defineDocumentType, makeSource } from 'contentlayer/source-files'
 import { defaultConfig } from 'next-docs-zeta/contentlayer/configuration'
 
-export default makeSource(defaultConfig)
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: `posts/**/*.md`,
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    seo: {
+      type: 'string',
+      required: true,
+    },
+    author: {
+      type: 'string',
+      required: true,
+    },
+    category: {
+      type: 'string',
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+    bannerImage: {
+      type: 'string',
+    },
+    tags: {
+      type: 'string',
+      required: true,
+    },
+    img: {
+      type: 'string',
+      required: true,
+    },
+    info: {
+      type: 'string',
+    },
+  },
+  computedFields: {
+    slug: {
+      type: 'string',
+      resolve: (doc) => doc._raw.flattenedPath.replace(/\.md$/, ''),
+    },
+  },
+}))
+
+export default makeSource({
+  ...defaultConfig,
+  documentTypes: [Post],
+})
