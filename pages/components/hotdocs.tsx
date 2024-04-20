7b7e7f98b325434ab3dfb22cb4b3a3069c5386be
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-20 22:38:18 +0800

    Update hotdocs.tsx

--- pages/components/hotdocs.tsx
+++ pages/components/hotdocs.tsx
@@ -7,7 +7,7 @@ function hotdocs() {
 <section className="bg-white dark:bg-gray-900 antialiased">
   <div className="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
     <div className="max-w-3xl mx-auto text-center">
-      <h2 className="text-4xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
+      <h2 className="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white">
         熱門文章
       </h2>
   
