f71b49e27ca198536e4ea6568c1bc8129ccc858b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-22 22:45:59 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -8,8 +8,8 @@ function table() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
       <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        雙龍體育<span <span className={styles.aabc}>公告</span>
-      </h2>
+  雙龍體育<span className={styles.aabc}>公告</span>
+</h2>
       <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
         這是雙龍體育最新重大公告。
       </p>
