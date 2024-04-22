06ae020d31ae286ab1a82d361691794e22727725
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-22 21:31:23 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -4,58 +4,131 @@ import styles from './Button.module.css';
 
 function table() {
   return (
-    <section style={{ backgroundColor: '#ffefd5' }}>
-      <Image
-          className="w-full"
-          src="/home.svg"
-          alt="dashboard image"
-        />
-  <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-    {/* 在電腦模式下顯示圖片在左側 */}
-    <div className="hidden md:block">
-      <Image
-        className="w-full"
-        src="/slb.png"
-        alt="雙龍職棒"
-      />
-    </div>
-    <div className="mt-4 md:mt-0">
-      <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white `}>
-        雙龍
-        <span className={styles.abc}>職棒</span>
+    <section className="bg-white dark:bg-gray-900">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+      <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        雙龍體育<span <span className={styles.aabc}>公告</span>
       </h2>
-      <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-        雙龍職籃是雙龍體育旗下的體育組織,總共有四支隊伍,第一屆尚未開打敬請期待
+      <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+        這是雙龍體育最新重大公告。
       </p>
-      <a
-        href="https://slb.ssangyongsports.eu.org/"
-        className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
-      >
-        更多資訊
-        <svg
-          className="ml-2 -mr-1 w-5 h-5"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </a>
     </div>
-    {/* 在手機模式下顯示圖片在下方 */}
-    <div className="md:hidden">
-      <Image
-        className="w-full"
-        src="/slb.png"
-        alt="雙龍職棒"
-      />
+    <div className="grid gap-8 lg:grid-cols-2">
+      <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex justify-between items-center mb-5 text-gray-500">
+          <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+            <svg
+              className="mr-1 w-3 h-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                clipRule="evenodd"
+              />
+              <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+            </svg>
+            公告
+          </span>
+          <span className="text-sm">2023-12-21</span>
+        </div>
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <Link href="/blog/close">雙龍體育暫停服務通知：我們的補償方案和未來展望</Link>
+        </h2>
+        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
+          比賽將在2024 6月重新開打，We will come back soon
+        </p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-4">
+            <img
+              className="w-7 h-7 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+              alt="Jese Leos avatar"
+            />
+            <span className="font-medium dark:text-white">Jese Leos</span>
+          </div>
+          <Link
+            href="/blog/close"
+            className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+          >
+            閱讀公告
+            <svg
+              className="ml-2 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </Link>
+        </div>
+      </article>
+      <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div className="flex justify-between items-center mb-5 text-gray-500">
+          <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
+            <svg
+              className="mr-1 w-3 h-3"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z"
+                clipRule="evenodd"
+              />
+              <path d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
+            </svg>
+            公告
+          </span>
+          <span className="text-sm">14 days ago</span>
+        </div>
+        <h2 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <Link href="/blog/slb">全新視覺形象 雙龍職棒推出新Logo</Link>
+        </h2>
+        <p className="mb-5 font-light text-gray-500 dark:text-gray-400">
+          全新視覺形象 雙龍職棒推出新Logo
+        </p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center space-x-4">
+            <img
+              className="w-7 h-7 rounded-full"
+              src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png"
+              alt="Bonnie Green avatar"
+            />
+            <span className="font-medium dark:text-white">Bonnie Green</span>
+          </div>
+          <a
+            href="#"
+            className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
+          >
+            Read more
+            <svg
+              className="ml-2 w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </a>
+        </div>
+      </article>
     </div>
   </div>
 </section>
+
  )
 }
 
