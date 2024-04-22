48bca03b88becc671e2ac55182fbb150d710c360
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-22 21:23:29 +0800

    Update Hero.tsx

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -3,31 +3,37 @@ import styles from './Button.module.css';
 import a from './a.module.css';
 function hero() {
   return (
-<div className="relative isolate px-6 pt-10 lg:px-8">
-  <div
-    className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-    aria-hidden="true"
-  >
-    <div
-      className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-      style={{
-        clipPath:
-          "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-      }}
-    />
-  </div>
-  <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-    <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-      <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-        雙龍體育暫停服務至6月通知{" "}
-        <Link href="#" className="font-semibold text-indigo-600">
-          <span className="absolute inset-0" aria-hidden="true" />
-          閱讀公告<span aria-hidden="true">→</span>
-        </Link>
-      </div>
-    </div>
-    <div className="text-center">
-      <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
+import Link from 'next/link';
+import styles from './Button.module.css';
+import a from './a.module.css';
+function hero() {
+  return (
+ <section className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+    <Link
+      href="/blog/close"
+      className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+    >
+      <span className="text-xs bg-primary-600 rounded-full text-white px-4 py-1.5 mr-3">
+        新
+      </span>{" "}
+      <span className="text-sm font-medium">
+        雙龍體育暫停服務至6月通知
+      </span>
+      <svg
+        className="ml-2 w-5 h-5"
+        fill="currentColor"
+        viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          fillRule="evenodd"
+          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+          clipRule="evenodd"
+        />
+      </svg>
+    </Link>
+    <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white"
         style={{ animationDelay: "0.3s", animationFillMode: "forwards" }}>
       我們的
      <span style={{ color: "red" }}>使命</span>
@@ -40,33 +46,46 @@ function hero() {
      <span className={styles.aabc}>最棒</span>
      體育組織
     </h2>
-      <div className="mt-10 flex items-center justify-center gap-x-6">
-        <Link
-          href="#anchor_one"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+      <Link
+  href="#anchor_one"
+  className="{a.fade-in-button} inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900 animate-fade-in"
+>
+        我們的體育
+        <svg
+          className="ml-2 -mr-1 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
         >
-              我們的體育
-        </Link>
-        <Link href="/tv" className="text-sm font-semibold leading-6 text-gray-900">
-          雙龍體育TV <span aria-hidden="true">→</span>
-        </Link>
-      </div>
+          <path
+            fillRule="evenodd"
+            d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+      <Link
+        href="/tv"
+        className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+      >
+        <svg
+          className="mr-2 -ml-1 w-5 h-5"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+        </svg>
+        加入TV
+      </Link>
     </div>
   </div>
-  <div
-    className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-    aria-hidden="true"
-  >
-    <div
-      className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-      style={{
-        clipPath:
-          "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-      }}
-    />
-  </div>
-</div>
+</section>
+  )
+}
 
+export default hero;
   )
 }
 
