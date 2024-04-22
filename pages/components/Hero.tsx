dbc4e32f9beb684a585ffd4c4c00c0568da1fdfa
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-22 21:20:07 +0800

    Update Hero.tsx

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css';
 import a from './a.module.css';
 function hero() {
   return (
-<div className="relative isolate px-6 pt-14 lg:px-8">
+<div className="relative isolate px-6 pt-10 lg:px-8">
   <div
     className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
     aria-hidden="true"
