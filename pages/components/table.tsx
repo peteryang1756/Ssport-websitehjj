cc5260d011182273eeba82dffe2684ddbc7b19a0
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-22 23:50:10 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -7,7 +7,7 @@ function table() {
     <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-           <h2 className={`mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 lg:text-center dark:text-white md:text-4xl `}>
+           <h2 className={`mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 dark:text-white md:text-4xl `}>
       最新
                    <span className={styles.abc}>消息</span>
 
