9b82358c3871186b52af44891e4d796dd750bbee
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-23 23:03:20 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -82,7 +82,7 @@ function C() {
             >
               內容
               <textarea
-                name="message"
+                name="Message"
                 rows={6}
                 required={true}
                 className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
