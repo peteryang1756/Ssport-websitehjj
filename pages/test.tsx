138a10e003357fa0a1d999d8d4fd16a30511a41d
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-27 19:22:49 +0800

    Create test.tsx

---
+++ pages/test.tsx
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import Link from 'next/link';
+
+export default function Home({ latestAnnouncements }) {
+  return (
+    <div>
+      <h2>Latest Announcements</h2>
+      <ul>
+        {latestAnnouncements.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>
+              <a>{post.frontmatter.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const files = fs.readdirSync('posts');
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const fullPath = path.join('posts', fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const { data: frontmatter } = matter(fileContents);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  const announcements = posts.filter((post) =>
+    post.frontmatter.tags.includes('公告')
+  );
+  const latestAnnouncements = announcements.slice(0, 3);
+
+  return {
+    props: {
+      latestAnnouncements,
+    },
+  };
+}
