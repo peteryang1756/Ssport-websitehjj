77645a5923c22f76cc390e5d104ae5c66205262e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-04-27 19:26:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,7 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import Link from 'next/link';
 import Hero from './components/Hero';
 import Table from './components/table';
 import Tabl from './components/tabl';
@@ -5,8 +9,7 @@ import Sport from './components/sport';
 import Action from './components/action';
 import React from 'react';
 import Head from 'next/head'
-
-const Home = () => {
+export default function Home({ latestAnnouncements }) {
   return (
     <>
 <Head>
@@ -24,8 +27,45 @@ const Home = () => {
 
         
       </div>
-    </>
+ 
+    <div>
+      <h2>Latest Announcements</h2>
+      <ul>
+        {latestAnnouncements.map((post) => (
+          <li key={post.slug}>
+            <Link href={`/blog/${post.slug}`}>
+              <a>{post.frontmatter.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+         </>
   );
 }
 
-export default Home;
+export async function getStaticProps() {
+  const files = fs.readdirSync('posts');
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace('.md', '');
+    const fullPath = path.join('posts', fileName);
+    const fileContents = fs.readFileSync(fullPath, 'utf8');
+    const { data: frontmatter } = matter(fileContents);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  const announcements = posts.filter((post) =>
+    post.frontmatter.tags.includes('公告')
+  );
+  const latestAnnouncements = announcements.slice(0, 3);
+
+  return {
+    props: {
+      latestAnnouncements,
+    },
+  };
+}
