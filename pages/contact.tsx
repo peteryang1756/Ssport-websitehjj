0359033d30241f4b316b39501349f35b709f41e9
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-05-07 18:26:33 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -50,9 +50,21 @@ function C() {
             <input type="text" name="honeypot" style={{ display: 'none' }} />
             <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
               <div>
-       <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">姓名</label>
-            <input type="text" id="first_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="John" required />
-       
+    
+  <label
+    htmlFor="first_name"
+    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+  >
+    姓名
+  </label>
+  <input
+    type="text"
+    id="first_name"
+    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+    placeholder="John"
+    required=""
+  />
+
               </div>
               <div>
                 <label
