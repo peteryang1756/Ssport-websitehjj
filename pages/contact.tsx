4b3f929fc693b1f8bb8e740b2e37b79cefd5faa0
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-05-12 15:18:31 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -19,131 +19,89 @@ function C() {
   return (
     <div>
       <section>
-        <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
-          <div
-            className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
-            aria-hidden="true"
-          >
-            <div
-              className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
-              style={{
-                clipPath:
-                  'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-              }}
-            ></div>
-          </div>
-          <div className="mx-auto max-w-2xl text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              聯繫我們
-            </h2>
-            <p className="mt-2 text-lg leading-8 text-gray-600">
-              有任何問題，立刻聯繫我們😊
-            </p>
-          </div>
+        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            <span className={styles.abc}>聯繫</span>
+            我們
+          </h2>
+          <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            有任何問題，立刻聯繫我們😊
+          </p>
           <form
             action="https://contact-api.ssangyongsports.eu.org"
             name="聯繫表單"
             method="POST"
-            className="mx-auto mt-16 max-w-xl sm:mt-20"
+            className="space-y-8"
           >
             {/* 在 HTML 表单中添加隐藏的 honeypot 字段 */}
             <input type="text" name="honeypot" style={{ display: 'none' }} />
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-    
-  <label
-    htmlFor="first_name"
-    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-  >
-    姓名
-  </label>
-  <input
-    type="text"
-    id="first_name"
-    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    placeholder="John"
-    
-  />
-
+            <label
+              htmlFor="name"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              你的電子郵件
+              <input
+                id="Email"
+                type="email"
+                name="Email"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="peter@ssangyongsports.org"
+                required={true}
+              />
+            </label>
+   
+            <div className="max-w-md" id="select">
+              <div className="mb-2 block">
+                <Label htmlFor="幫助範圍" value="幫助範圍" />
               </div>
-              <div>
-                <label
-                  htmlFor="Email"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  你的電子郵件
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    id="Email"
-                    type="email"
-                    name="Email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="peter@ssangyongsports.org"
-                    required={true}
-                  />
-                </div>
-              </div>
-
-              <div className="sm:col-span-2">
-                <div className="mb-2 block">
-                  <Label htmlFor="幫助範圍" value="幫助範圍" />
-                </div>
-                <Select name="help" required>
-                  <option value="雙龍體育">雙龍體育</option>
-                  <option value="雙龍職籃">雙龍職籃</option>
-                  <option value="雙龍職棒">雙龍職棒</option>
-                  <option value="其他">其他</option>
-                </Select>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="Subject"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  主旨
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    id="Subject"
-                    type="text"
-                    name="Subject"
-                    defaultValue={subjectDefaultValue} // 在此處設置預設值
-                    required={true}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                    placeholder="讓我們得知如何幫助你"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  內容
-                </label>
-                <div className="mt-2.5">
-                <Textarea name="message" id="message" placeholder="撰寫..." required rows={4} />
-                </div>
-              </div>
-            
-            <div className="mt-10">
-              <button
-                type="submit"
-                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                發送訊息
-              </button>
+              <Select name="help" required>
+                <option value="雙龍體育">雙龍體育</option>
+                <option value="雙龍職籃">雙龍職籃</option>
+                <option value="雙龍職棒">雙龍職棒</option>
+                <option value="其他">其他</option>
+              </Select>
             </div>
+            <label
+              htmlFor="Subject"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              主旨
+              <input
+                id="Subject"
+                type="text"
+                name="Subject"
+                defaultValue={subjectDefaultValue} // 在此處設置預設值
+                required={true}
+                className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="讓我們得知如何幫助你"
+              />
+            </label>
+            <label
+              htmlFor="message"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+            >
+              內容
+              <textarea
+                name="message"
+                rows={6}
+                required={true}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+                defaultValue={""}
+              />
+            </label>
+            <button
+              type="submit"
+              className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+            >
+              發送訊息
+            </button>
           </form>
         </div>
       </section>
       <Head>
         <title>雙龍體育聯絡</title>
-        <meta
-          name="description"
-          content="雙龍體育聯絡，為你提供專業的雙龍體育客服，為你解決問題，或聯絡雙龍體育了解你想得知的消息，就立刻上雙龍體育聯絡網站吧"
-        />
+        <meta name="description" content="雙龍體育聯繫，為你提供專業的雙龍體育客服，為你解決問題，或聯絡雙龍體育了解你想得知的消息，就立刻上雙龍體育聯絡網站吧" />
       </Head>
     </div>
   );
