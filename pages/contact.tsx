756776a25053e4cbd59a60e4a8b05d03fb5071c5
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-05-16 18:24:18 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -39,6 +39,21 @@ function C() {
               htmlFor="name"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
             >
+              你的名稱
+              <input
+                id="Name"
+                type="name"
+                name="Name"
+                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="讓我們知道你是誰"
+                required={true}
+              />
+            </label>
+   
+            <label
+              htmlFor="mail"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
               你的電子郵件
               <input
                 id="Email"
