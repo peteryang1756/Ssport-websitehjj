1bfc52a6b5f98846ed8f84878d9d18e74ae3c298
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2024-05-23 16:20:43 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -29,7 +29,6 @@ export default function Post({frontmatter, content}) {
       <Breadcrumb.Item href={`/blog/tags/${tags}`}>{tags}</Breadcrumb.Item>
 
     </Breadcrumb>
-   <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
       <h1 className="mb-4 text-4xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-5xl dark:text-white">
@@ -64,7 +63,6 @@ export default function Post({frontmatter, content}) {
       </header>
     </article>
   </div>
-</main>
 
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
       <header className="mb-4 lg:mb-6 not-format">
